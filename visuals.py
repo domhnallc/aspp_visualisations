63996a07757112c7c26e2fa3a8ee9e0a31b97d91
domhnallc <d.carlin@qub.ac.uk>
2022-09-15 16:44:57 +0100

    finished visualisation

--- visuals.py
+++ visuals.py
@@ -3,6 +3,7 @@ import pandas as pd
 import scipy
 import jinja2
 import tomli
+import seaborn as sns
 
 
 
@@ -37,8 +38,8 @@ def vis_unis_with_sware(df_base):
     plt.pie(vals, labels=labels, autopct="%1.1f%%", explode=explode)
     plt.title("Software contained in \nUK Academic Institutional Repositories")
     plt.axis("equal")
-    plt.savefig("/home/domhnall/Dev/aspp/visualisation/insts_category.pdf")
-    print("Saved /home/domhnall/Dev/aspp/visualisation/insts_category.pdf")
+    plt.savefig("./insts_category.pdf")
+    print("Saved ./insts_category.pdf")
     # plt.show()
 
 
@@ -48,6 +49,8 @@ def vis_contains_sware_by_ris_type(df_base):
         columns=df_base["Category"],
         normalize="index",
     ).sort_values("Contains software")
+
+    print(cross_tab_prop.to_latex())
     print(cross_tab_prop)
 
     cross_tab_prop.plot(kind="barh", stacked=True)
@@ -67,6 +70,7 @@ def vis_metdata_format_by_contains_sware(df_base):
     ).sort_values("Contains software")
     print(cross_tab_prop)
 
+    print(cross_tab_prop.to_latex())
     cross_tab_prop.plot(kind="barh", stacked=True)
     plt.title(
         "Proportion of repositories that (a) contain software records,\n (b) contain no software records but "
@@ -92,45 +96,19 @@ def chisq(subhead, cross_tab_prop):
 
 
 def vis_unis_with_sware_barchart(df_filtered):
-    df_sware_unis = df_filtered.query("Manual_Num_sw_records >0")
+    df_sware_unis = df_filtered.sort_values(by=['Manual_Num_sw_records']).query("Manual_Num_sw_records >0")
     print(df_sware_unis)
     df_sware_unis.plot(kind="barh")
     plt.title("Number of software records >0 per institution")
     plt.xlabel("Software records")
     plt.grid(which="major", linestyle="-", linewidth="0.5", color="black")
-    plt.grid(which="minor", linestyle=":", linewidth="0.25", color="black")
+    plt.grid(which="minor", linestyle=":", linewidth="0.5", color="black")
     plt.minorticks_on
-    plt.savefig("/home/domhnall/Dev/aspp/visualisation/sware_recs_per_inst.pdf")
+    plt.savefig("./sware_recs_per_inst.pdf")
     plt.show()
 
 
 def vis_russell_group_correlation(df_russell):
-    russell = [
-        "University of Birmingham",
-        "University of Bristol",
-        "University of Cambridge",
-        "Cardiff University",
-        "Durham University",
-        "University of Edinburgh",
-        "University of Exeter",
-        "University of Glasgow",
-        "Imperial College London",
-        "King's College London",
-        "University of Leeds",
-        "University of Liverpool",
-        "London School of Economics & Political Science",
-        "University of Manchester",
-        "Newcastle University",
-        "University of Nottingham",
-        "University of Oxford",
-        "Queen Mary, University of London",
-        "Queen's University Belfast",
-        "University of Sheffield",
-        "University of Southampton",
-        "University College London",
-        "University of Warwick",
-        "University of York",
-    ]
 
     print(df_russell.sort_values("uni_sld"))
 
@@ -141,7 +119,9 @@ def vis_russell_group_correlation(df_russell):
     ).sort_values("Contains software")
 
     print(russell_cross_tab_prop)
+    print(russell_cross_tab_prop.to_latex())
     russell_cross_tab_prop.plot(kind="barh", stacked=True)
+    plt.title("Crosstabulation of membership of Russell group with software records in repository.")
     plt.show()
 
     return russell_cross_tab_prop
@@ -167,10 +147,18 @@ def correlate_sware_rse_groups(df: pd.DataFrame):
         normalize="index",
     ).sort_values("RSE_group")
     print(cross_tab_prop)
+    print(cross_tab_prop.to_latex())
+
 
     return cross_tab_prop
-    
 
+def vis_crosstab_heatmap(crosstab: pd.crosstab, title: str):
+
+    plt.figure(figsize=(12,12))
+    plt.title(title)
+    sns.heatmap(crosstab, cmap='crest')
+    plt.show()
+    
 def main():
     pd.set_option("display.max_rows", None)
     pd.set_option("display.max_columns", None)
@@ -178,30 +166,31 @@ def main():
     # load and prep file
     df_all_data = get_dataframe(data_file, sort_key='name')
     df_filtered = filter_dataframe(df_all_data)
-    # vis_unis_with_sware(df_filtered)
-    # vis_unis_with_sware(df_filtered)
+    vis_unis_with_sware(df_filtered)
+    vis_unis_with_sware(df_filtered)
 
     # vis software by RIS type
-    # vis_contains_sware_by_ris_type(df_filtered)
+    vis_contains_sware_by_ris_type(df_filtered)
 
     # vis software by metadata format
-    # vis_metdata_format_by_contains_sware(df_filtered)
+    vis_metdata_format_by_contains_sware(df_filtered)
 
     # bar chart of unis with software only
-    # vis_unis_with_sware_barchart(df_filtered)
+    vis_unis_with_sware_barchart(df_filtered)
 
     # Correlate Russell Group members with s'ware
-    #df_russell = get_dataframe("./russell_sld_sware.csv", sort_key="uni_sld")
-    #russell_top_20_sw = df_russell.sort_values("Manual_Num_sw_records", ascending=False).head(20)
-    #print("\n\nTop 20 Universities in order of number of software records in repository, and membership of Russell Group\n\n",russell_top_20_sw)
-    #russell_ctp = vis_russell_group_correlation(df_russell)
-    #chisq(subhead="Membership of Russell Group vs Software in repository", cross_tab_prop=russell_ctp)
+    df_russell = get_dataframe("./russell_sld_sware.csv", sort_key="uni_sld")
+    russell_top_20_sw = df_russell.sort_values("Manual_Num_sw_records", ascending=False).head(20)
+    print("\n\nTop 20 Universities in order of number of software records in repository, and membership of Russell Group\n\n",russell_top_20_sw)
+    russell_ctp = vis_russell_group_correlation(df_russell)
+    chisq(subhead="Membership of Russell Group vs Software in repository", cross_tab_prop=russell_ctp)
+    vis_crosstab_heatmap(russell_ctp, "Heatmap of cross tabulation between Russell Group Membership and software records in repository.")
 
     # Correlate RSE groups with s'ware
     print(df_filtered.keys())
     df_rse_groups = df_filtered[['RSE_group','Category']]
     groups_cross_tab = correlate_sware_rse_groups(df_rse_groups)
     chisq(cross_tab_prop=groups_cross_tab, subhead="RSE Groups vs sware records")
-
+    vis_crosstab_heatmap(groups_cross_tab, "Heatmap of cross tabulation between RSE Group present in Institute and sofware records in repository.")
 
 main()
